01476936d7b4ea217068e01504997da9b3489343
bongcoy <mpascaldewantara@gmail.com>
2024-02-16 17:33:00 +0700

    update user.dart

--- lib/features/home/domain/entities/user.dart
+++ lib/features/home/domain/entities/user.dart
@@ -1,17 +1,27 @@
 import 'dart:convert';
 
-class User {
+import 'package:equatable/equatable.dart';
+
+class User extends Equatable {
   final String id;
   final String name;
   final String avatar;
   final DateTime createdAt;
 
-  User({
+  const User({
     required this.id,
     required this.name,
     required this.avatar,
     required this.createdAt,
   });
+
+  User.empty()
+      : this(
+    id: '_empty.id',
+    name: '_empty.name',
+    avatar: '_empty.avatar',
+    createdAt: DateTime.now(),
+  );
 
   factory User.fromRawJson(String str) => User.fromJson(json.decode(str));
 
@@ -30,4 +40,7 @@ class User {
         "avatar": avatar,
         "createdAt": createdAt.toIso8601String(),
       };
+
+  @override
+  List<Object?> get props => throw UnimplementedError();
 }
