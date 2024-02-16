cfe1021ec5d45dea41f5eb58255abdbb938c7959
bongcoy <mpascaldewantara@gmail.com>
2024-02-16 17:32:04 +0700

    add auth_repo.dart

---
+++ lib/features/home/domain/repositories/auth_repo.dart
@@ -0,0 +1,17 @@
+import 'package:dartz/dartz.dart';
+import 'package:dummy_tdd_clean/core/errors/failure.dart';
+import 'package:dummy_tdd_clean/core/utilities/typedef.dart';
+import 'package:dummy_tdd_clean/features/home/domain/entities/user.dart';
+
+abstract class AuthRepo {
+  const AuthRepo();
+
+  ResultFuture<void> createUser({
+    required String id,
+    required String name,
+    required String avatar,
+    required String createdAt,
+  });
+
+  ResultFuture<List<User>> getUsers();
+}
