60a3c0787e09dfdf6c77ef6e3455840fcaa976d1
bongcoy <mpascaldewantara@gmail.com>
2024-02-21 17:33:17 +0700

    update implement function

--- lib/features/authentication/data/data_sources/auth_remote_data_source.dart
+++ lib/features/authentication/data/data_sources/auth_remote_data_source.dart
@@ -1,3 +1,8 @@
+import 'dart:convert';
+
+import 'package:dummy_tdd_clean/core/errors/exceptions.dart';
+import 'package:dummy_tdd_clean/core/utilities/constants.dart';
+import 'package:dummy_tdd_clean/core/utilities/typedef.dart';
 import 'package:dummy_tdd_clean/features/authentication/data/models/user_model.dart';
 import 'package:http/http.dart' as http;
 
@@ -13,7 +18,6 @@ abstract class AuthRemoteDataSource {
 }
 
 class AuthRemoteDataSourceImpl implements AuthRemoteDataSource {
-
   AuthRemoteDataSourceImpl({required http.Client client}) : _client = client;
 
   final http.Client _client;
@@ -24,14 +28,51 @@ class AuthRemoteDataSourceImpl implements AuthRemoteDataSource {
     required String name,
     required String avatar,
     required String createdAt,
-  }) {
-    // TODO: implement createUser
-    throw UnimplementedError();
+  }) async {
+    try {
+      final http.Response response = await _client.post(
+        Uri.https(baseurl,kPostUsers),
+        body: jsonEncode(
+          {
+            'id': 'id',
+            'name': 'name',
+            'avatar': 'avatar',
+            'createdAt': 'createdAt',
+          },
+        ),
+      );
+
+      if (response.statusCode != 200 && response.statusCode != 201) {
+        throw APIException(
+            message: response.body, statusCode: response.statusCode);
+      }
+    } on APIException {
+      rethrow;
+    } catch (e) {
+      // this error is probably coming from developer
+      throw APIException(message: e.toString(), statusCode: 505);
+    }
   }
 
   @override
-  Future<List<UserModel>> getUsers() {
-    // TODO: implement getUsers
-    throw UnimplementedError();
+  Future<List<UserModel>> getUsers() async {
+    try {
+      final http.Response response =
+          await _client.get(Uri.https(baseurl, kGetUsers));
+
+      if (response.statusCode != 200) {
+        throw APIException(
+            message: response.body, statusCode: response.statusCode);
+      }
+
+      return List<DataMap>.from(jsonDecode(response.body) as List)
+          .map((e) => UserModel.fromMap(e))
+          .toList();
+    } on APIException {
+      rethrow;
+    } catch (e) {
+      // this error is probably coming from developer
+      throw APIException(message: e.toString(), statusCode: 505);
+    }
   }
 }
