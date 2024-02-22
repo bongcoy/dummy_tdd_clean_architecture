3ff55d9881ccaa1ea388c009a9acaf0c39f19d6f
bongcoy <mpascaldewantara@gmail.com>
2024-02-22 09:52:26 +0700

    update add errorMessage getter

--- lib/core/errors/failure.dart
+++ lib/core/errors/failure.dart
@@ -2,10 +2,12 @@ import 'package:dummy_tdd_clean/core/errors/exceptions.dart';
 import 'package:equatable/equatable.dart';
 
 abstract class Failure extends Equatable {
+  const Failure({required this.message, required this.statusCode});
+
   final String message;
   final int statusCode;
 
-  const Failure({required this.message, required this.statusCode});
+  String get errorMessage => '$statusCode Error: $message';
 
   @override
   List<Object?> get props => [message, statusCode];
