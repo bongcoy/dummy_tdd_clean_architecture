393a7ddc2980084dcb1755d816d4b8dbbee0261f
bongcoy <mpascaldewantara@gmail.com>
2024-02-22 10:01:34 +0700

    add cubit manager

---
+++ lib/features/authentication/presentation/manager/cubit/authentication_cubit.dart
@@ -0,0 +1,51 @@
+import 'package:bloc/bloc.dart';
+import 'package:dummy_tdd_clean/features/authentication/domain/entities/user.dart';
+import 'package:dummy_tdd_clean/features/authentication/domain/use_cases/create_user.dart';
+import 'package:dummy_tdd_clean/features/authentication/domain/use_cases/get_users.dart';
+import 'package:equatable/equatable.dart';
+import 'package:meta/meta.dart';
+
+part 'authentication_state.dart';
+
+class AuthenticationCubit extends Cubit<AuthenticationState> {
+  AuthenticationCubit(
+      {required CreateUser createUser, required GetUsers getUsers})
+      : _createUser = createUser,
+        _getUsers = getUsers,
+        super(const AuthenticationInitial()) {}
+
+  final CreateUser _createUser;
+  final GetUsers _getUsers;
+
+  Future<void> _createUserHandler({
+    required String id,
+    required String name,
+    required String avatar,
+    required DateTime createdAt,
+  }) async {
+    emit(const CreatingUser());
+    final result = await _createUser(CreateUserParams(
+      id: id,
+      name: name,
+      avatar: avatar,
+      createdAt: createdAt,
+    ));
+
+    result.fold(
+      (l) => emit(AuthenticationError(message: l.errorMessage)),
+      (r) => emit(const UserCreated()),
+    );
+  }
+
+  Future<void> _getUserHandler(
+    Emitter<AuthenticationState> emitter,
+  ) async {
+    emit(const GettingUsers());
+    final result = await _getUsers();
+
+    result.fold(
+      (failure) => emit(AuthenticationError(message: failure.errorMessage)),
+      (users) => emit(UsersLoaded(users: users)),
+    );
+  }
+}
---
+++ lib/features/authentication/presentation/manager/cubit/authentication_state.dart
@@ -0,0 +1,44 @@
+part of 'authentication_cubit.dart';
+
+@immutable
+
+abstract class AuthenticationState extends Equatable {
+  const AuthenticationState();
+
+  @override
+  List<Object> get props => [];
+}
+
+class AuthenticationInitial extends AuthenticationState {
+  const AuthenticationInitial();
+}
+
+class CreatingUser extends AuthenticationState {
+  const CreatingUser();
+}
+
+class GettingUsers extends AuthenticationState {
+  const GettingUsers();
+}
+
+class UserCreated extends AuthenticationState {
+  const UserCreated();
+}
+
+class UsersLoaded extends AuthenticationState {
+  const UsersLoaded({required this.users});
+
+  final List<User> users;
+
+  @override
+  List<Object> get props => users.map((user) => user.id).toList();
+}
+
+class AuthenticationError extends AuthenticationState {
+  const AuthenticationError({required this.message});
+
+  final String message;
+
+  @override
+  List<Object> get props => [message];
+}
